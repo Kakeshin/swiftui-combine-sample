2d1590cde898659ffd8e0031cd0ab9ff5675a72d
Kakeshin <kakeshin.engineer@gmail.com>
2023-09-29 23:18:57 +0900

    Add: project

---
+++ swiftui-combine-sample/swiftui_combine_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_combine_sampleApp.swift
+//  swiftui-combine-sample
+//
+//  Created by Kakeshin on 2023/09/29.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_combine_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
